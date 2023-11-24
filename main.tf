8b261f3b8fd29904bbb9532d3b0b932a12593f01
Rajasekaran S <proffraja@gmail.com>
2023-11-24 15:30:07 +0530

    updated

--- main.tf
+++ main.tf
@@ -8,6 +8,15 @@ resource "google_storage_bucket" "gcs_bucket" {
   location = "europe-west2"
 }
 
+provider "google" {
+  credentials = file(var.credentials_file)
+
+  project = var.project
+  region  = var.region
+  zone    = var.zone
+}
+
+
 resource "google_compute_instance" "vm_instance" {
   name         = "rajasekaran-tf-vm-instance"
   machine_type = "f1-micro"
