0509ccece80b1a1d3e3a0123e146f44902a82f82
Sandeep Joshi <sandeep@kognitos.com>
2023-11-24 09:37:07 +0530

    change name

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "google_storage_bucket" "bucket" {
-  name     = "test-bucket-random-1234"
+  name     = "test-bucket-kite-project-1234"
   location = "europe-west2"
 }
 
 resource "google_storage_bucket" "gcs_bucket" {
-  name     = "test-bucket-random-5678"
+  name     = "test-bucket-kite-project-5678"
   location = "europe-west2"
 }
