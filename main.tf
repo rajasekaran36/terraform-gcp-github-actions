57c79c001604c37570edc2f144316ad5417de940
Rajasekaran S <proffraja@gmail.com>
2023-11-24 15:26:31 +0530

    gpc

--- main.tf
+++ main.tf
@@ -7,3 +7,20 @@ resource "google_storage_bucket" "gcs_bucket" {
   name     = "test-bucket-raja-project-5678"
   location = "europe-west2"
 }
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "rajasekaran-tf-vm-instance"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+}
