5ad21ce684c067aea3f815ea98a8ebce0731c428
Rajasekaran S <proffraja@gmail.com>
2023-11-24 15:15:22 +0530

    updated

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "google_storage_bucket" "bucket" {
-  name     = "test-bucket-rajasekaran2-project-1234"
+  name     = "test-bucket-raja-project-1234"
   location = "europe-west2"
 }
 
 resource "google_storage_bucket" "gcs_bucket" {
-  name     = "test-bucket-rajasekaran2-project-5678"
+  name     = "test-bucket-raja-project-5678"
   location = "europe-west2"
 }
